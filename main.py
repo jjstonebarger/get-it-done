2e17ce1fbfbd8a3d5c1286c9d4422a74b6020c32
Justin Stonebarger <jjstonebarger@gmail.com>
2018-10-05 21:39:30 -0600

    Completed Steps 5-8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:AmOa5gNUbYseqfFx@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Task(db.Model):
 
@@ -16,6 +17,64 @@ class Task(db.Model):
     def __init__(self,name):
         self.name = name
         self.completed = False
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self,email,password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect ('/login')
+
+
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session ['email'] = email
+            flash("Logged in")
+            return redirect ('/')
+
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html') 
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form ['verify']
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session ['email'] = email
+            return redirect ('/')
+        else:
+            return "<h1>Duplicate user</h1>"
+
+    return render_template('register.html') 
+
+@app.route('/logout')
+def logout():
+    del session ['email']
+    return redirect ('/')
 
 
 @app.route('/', methods=['POST', 'GET'])
